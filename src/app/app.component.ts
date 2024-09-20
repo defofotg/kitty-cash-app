a01ebfa75efd29b6b70f2c553ef12c06210bd3e9
Georges Ledoux DEFO FOTSI <defo.georges@gmail.com>
2024-09-20 20:52:26 +0200

    Initial commit
    wip

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ]
+})
+export class AppComponent {
+  title = 'kitty-cash';
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  api: "http://209.182.238.71:1080/api"
+};

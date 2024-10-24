612113f6b91f885c7f59e10b5ecc5265a20c2db8
junior-gerome <jnguepmanyo@gmail.com>
2024-10-24 19:10:40 +0100

    Creation du menu de navigation
    amelioration du de navigation
    
    modification de routerlinkActive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet, CommonModule, LoginComponent],
+  imports: [RouterOutlet, CommonModule, LoginComponent, NavbarComponent],
 })
 export class AppComponent {
-  title = 'kitty-cash';
+
 }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,47 @@
+import { Component, HostListener } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  imports: [CommonModule,RouterModule],
+})
+export class NavbarComponent {
+  user = { firstName: 'george', lastName: 'Defo' };
+  isDropdownOpen = false;
+  
+
+  constructor(private router: Router) {}
+
+//Méthode qui renvoie les initiales de l'utilisateur ex:GD.
+  getInitials(): string {
+    const { firstName, lastName } = this.user;
+    return `${firstName.charAt(0)}${lastName.charAt(0)}`.toUpperCase();
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+   
+   @HostListener('document:click', ['$event'])
+   handleOutsideClick(event: Event) {
+     const elementCible = event.target as HTMLElement;
+     const ClickdansMenu = elementCible.closest('.dropdown');
+ 
+     if (!ClickdansMenu && this.isDropdownOpen) {
+       this.isDropdownOpen = false;
+     }
+   }
+
+
+   logout() {
+    localStorage.removeItem('userToken');
+    this.router.navigate(['/login']);
+  }
+
+ 
+}

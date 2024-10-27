6f81f61e7b354fdc8f3c180b0d860ad1139e633a
sadodimitri861@gmail.com <sadodimitri861@gmail.com>
2024-10-27 09:35:22 +0100

    creation de la page de connexion et des composant login, buttom, input et saturation des classe der tailwind

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
+import { FormComponent } from '../../shared/components/form/form.component';
+import { InputComponent } from '../../shared/components/input/input.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormComponent,
+    InputComponent,
+    ReactiveFormsModule,
+    ButtonComponent,
+    CommonModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  backgroundImage = 'assets/images/login-walkpaper.png';
+
+  formGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    chekbox: new FormControl(false, [Validators.requiredTrue]),
+  });
+
+  constructor() {
+    this.formGroup.valueChanges.subscribe(() => console.log(this.formGroup));
+  }
+
+  /**to gerer les deux fonction
+   */
+  onSubmit() {
+    if (this.formGroup.valid) {
+      console.log('Données du formulaire :', this.formGroup.value);
+    } else {
+      console.log('Le formulaire est invalide');
+    }
+  }
+
+  loading = false;
+
+  handleButtonClick() {
+    this.loading = true;
+
+    // Simule une requête API ou autre action
+    setTimeout(() => {
+      this.loading = false;
+      console.log('Action du bouton exécutée');
+    }, 2000);
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,133 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
+
+    theme: {
+      /*modification de tailwind.js*/
+  
+      colors: {
+        white: '#FFFFFF',
+        black: '100e34',
+        primary: {
+            600: '#ffd35f',
+            DEFAULT: '#ffbf18',
+        },
+        secondary: {
+          200: '#28264c', //opacity 4%
+          400: '#cbd5e1',
+          600: '#535177',
+          DEFAULT: '#28264c',
+        },
+        tertiary: {
+          DEFAULT: '#4f48ec',
+        },
+        alert: {
+          danger: "#FF4E4E",
+          success: "#90DA1A",
+          warning: "#FEB72F",
+        }
+      },
+  
+      fontSize: {
+        '8xl': ['120px', { /* pour display*/
+          lineHeight: '120px',
+          letterSpacing: '-6px',
+          fontWeight: '500',
+        }],
+        
+        '7xl': ['70px', { /*pour h1 */
+          lineHeight: '80px',
+          letterSpacing: '-4.5px',
+          fontWeight: '600',
+        }],
+  
+        '6xl': ['55px', { /*pour h2 */
+          lineHeight: '60px',
+          letterSpacing: '-2.5px',
+          fontWeight: '500',
+        }],
+  
+        '5xl': ['48px', { /*pour h3 */
+          lineHeight: '54px',
+          letterSpacing: '-1.6px',
+          fontWeight: '500',
+        }],
+  
+        '4xl': ['36px', { /*pour h4 */
+          lineHeight: '44px',
+          letterSpacing: '-1.2px',
+          fontWeight: '500',
+        }],
+  
+        '3xl': ['28px', { /*pour h5 */
+          lineHeight: '34px',
+          letterSpacing: '-0.8px',
+          fontWeight: '500',
+        }],
+  
+        '2xl': ['24px', { /*pour lead regular */
+          lineHeight: '30px',
+          letterSpacing: '-1px',
+          fontWeight: '400',
+        }],
+  
+        'xl': ['24px', { /*pour lead medium */
+          lineHeight: '30px',
+          letterSpacing: '-1px',
+          fontWeight: '400',
+        }],
+  
+        'lg': ['21px', { /*pour le body-lg*/
+          lineHeight: '30px',
+          letterSpacing: '-0.800000011920929px',
+          fontWeight: '400',
+        }],
+  
+        'base': ['17px', { /*pour le body-base*/
+          lineHeight: '25px',
+          letterSpacing: '-0.69999998079071px',
+          fontWeight: '400',
+        }],
+  
+        'sm': ['15px', { /*pour le body-sm*/
+          lineHeight: '23px',
+          letterSpacing: '-0.6000000238418579px',
+          fontWeight: '400',
+        }],
+  
+        'caption1': ['20px', { 
+          lineHeight: '24px',
+          letterSpacing: '-0.6000000238418579px',
+          fontWeight: '400',
+        }],
+  
+  
+        'caption2': ['18px', { 
+          lineHeight: '20px',
+          letterSpacing: '-0.30000001192092896px',
+          fontWeight: '400',
+        }],
+  
+  
+        'caption3': ['16px', { 
+          lineHeight: '18px',
+          letterSpacing: '-0.5px',
+          fontWeight: '400',
+        }],
+  
+  
+        'caption4': ['13px', { 
+          lineHeight: '15px',
+          letterSpacing: '-0.2000000298023224px',
+          fontWeight: '400',
+        }],
+      },
+  
+      borderRadius: {
+      DEFAULT: '10px',
+      full: '100%',
+      },
+
     extend: {
       fontFamily: {
         sans: ['"Inter"', ...defaultTheme.fontFamily.sans]
@@ -17,4 +144,4 @@ module.exports = {
     require('flowbite/plugin')
   ],
 }
-
+}
